446667d834e6a228c34e6e2e23a3820f636b0664
Bruce <BruceLuu007@gmail.com>
2018-08-19 23:49:52 +0800

    人工智能第2、3步

--- SingleGame.cpp
+++ SingleGame.cpp
@@ -16,24 +16,47 @@ void SingleGame::click(int id,int row,int col){
 //3.评估走的结果
 //4.取最好的结果
 void SingleGame::computerMove(){
-    //第一步
+    //第1步
     QVector<Step*>steps;
     getAllPossibleMove(steps);
+    //第2步
+    int maxScore=INT_MIN;
+    Step* ret;
+    for(auto it=steps.begin();it!=steps.end();++it){
+        Step*step=*it;
+        fakeMove(step);//假走一下
+        int score=calcScore();
+        unfakeMove(step);//复原
+        if(score>maxScore){
+            maxScore=score;
+            ret=step;
+        }
+    }
+    //第4步
+    //return ret;
 
 }
 
 void SingleGame::getAllPossibleMove(QVector<Step*>&steps){
     //遍历所有黑棋看看走法
-    for(int i=16;i!=32;++i){
-        for(int row=0;row!=10;++row){
+    for(int i=16;i!=32;++i)
+        for(int row=0;row!=10;++row)
             for(int col=0;col!=9;++col){
                 int killid=this->getStoneId(row,col);
                 if(sameColor(killid,i))continue;
-                if(canMove(i,row,col,killid)){
-                    //可以走 保存一下
-                    saveStep(i,killid,row,col,steps);
-                }
+                if(canMove(i,row,col,killid))
+                    saveStep(i,killid,row,col,steps);//可以走 保存一下
             }
-        }
-    }
+}
+
+
+void SingleGame::fakeMove(Step*step){
+    killStone(step->_killid);
+    moveStone(step->_moveid,step->_rowTo,step->_colTo);
+}
+void SingleGame::unfakeMove(Step*step){
+
+}
+int SingleGame::calcScore(){
+
 }
--- SingleGame.h
+++ SingleGame.h
@@ -8,6 +8,9 @@ public:
     virtual void click(int,int,int);//ÈË¹¤ÖÇÄÜ¡î
     void computerMove();
     void getAllPossibleMove(QVector<Step*>& steps);
+    void fakeMove(Step*step);
+    void unfakeMove(Step*step);
+    int calcScore();
 };
 
 #endif // SINGLEGAME_H
